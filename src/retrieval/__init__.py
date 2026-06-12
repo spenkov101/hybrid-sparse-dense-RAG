ec602c58bc86dc7b0a03aa90b7a0f09f74692be6
Stanislav Penkov <spenkov101@gmail.com>
2026-06-12 17:15:04 +0300

    refactor: add retrieval package exports

---
+++ src/retrieval/__init__.py
@@ -0,0 +1,9 @@
+from .dense import DenseRetriever
+from .splade import SpladeRetriever
+from .hybrid_retriever import HybridRetriever
+
+__all__ = [
+    "DenseRetriever",
+    "SpladeRetriever",
+    "HybridRetriever",
+]
